45b1f5713afe7c428effd524c674b5b291eb70a6
Anh Hao Huynh Huy <anhhaohuynhhuy2018@gmail.com>
2023-08-16 22:16:52 +0700

    th1

---
+++ bt6/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Home Page')),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Home Screen",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 20),
+              ColorfulBoxes(),
+              const SizedBox(height: 20),
+              Image.network(
+                'https://i0.wp.com/s3.ap-southeast-1.amazonaws.com/techover.storage/wp-content/uploads/2022/03/31225751/dart-logo-for-shares-2-300x168.png?resize=300%2C168&ssl=1',
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+              ),
+              Image.network(
+                'https://media.techmaster.vn/api/static/brbgh4451coepbqoch60/Lf-bvYrA',
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+              ),
+              Image.network(
+              'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/android_logo.jpg',
+                width: MediaQuery.of(context).size.width,
+                height: 150,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Click here'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorfulBoxes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ColorBox(color: Colors.blue),
+        ColorBox(color: Colors.green),
+        ColorBox(color: Colors.orange),
+      ],
+    );
+  }
+}
+
+class ColorBox extends StatelessWidget {
+  final Color color;
+
+  ColorBox({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: color,
+    );
+  }
+}
